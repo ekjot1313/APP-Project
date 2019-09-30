f339ecf2073e0b99b8c0eda1a74ff1e2335068e0
ekjot <ekjotsingh00776@gmail.com>
2019-09-30 16:09:29 -0400

    Ekjot- added temporary MapEditor class

--- src/MapEditor/MapEditor.java
+++ src/MapEditor/MapEditor.java
@@ -1,5 +1,19 @@
 package MapEditor;
+/**
+ * Temporary class for user driven commands to edit map
+ * @author ekjot
+ *
+ */
 
-public class mapEditor {
+import java.io.*;
+
+public class MapEditor {
+
+	public static void main(String[] args) throws IOException {
+		// We need to provide file path as the parameter:
+		// double backquote is to avoid compiler interpret words
+		// like \test as \t (ie. as a escape sequence)
+
+	}
 
 }
