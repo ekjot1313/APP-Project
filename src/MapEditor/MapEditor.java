0859dc1bb4ce35c8fda9fcccc6795c956040f719
ekjot <ekjotsingh00776@gmail.com>
2019-09-30 16:58:21 -0400

    Ekjot-MapEditor class now can read user commands

--- src/MapEditor/MapEditor.java
+++ src/MapEditor/MapEditor.java
@@ -1,6 +1,8 @@
 package MapEditor;
 /**
- * Temporary class for user driven commands to edit map
+ * Temporary class for user driven commands to edit map.
+ * This will be called when user will enter 'editmap' command
+
  * @author ekjot
  *
  */
@@ -10,11 +12,72 @@ import java.io.*;
 public class MapEditor {
 
 	public static void main(String[] args) throws IOException {
-		// We need to provide file path as the parameter:
-		// double backquote is to avoid compiler interpret words
-		// like \test as \t (ie. as a escape sequence)
-		
-		String ab;
+
+		BufferedReader brConsole = new BufferedReader(new InputStreamReader(System.in));
+
+		String command[];
+
+		int len;
+
+		command = brConsole.readLine().split(" ");
+		len = command.length;
+
+		while (!command[0].equals("savemap")) {
+			
+			switch (command[0]) {
+			case "editcontinent": {
+				editContinent(command);
+
+				break;
+			}
+			case "editcountry": {
+				editCountry(command);
+				break;
+			}
+			case "editneighbor": {
+				editNeighbour(command);
+				break;
+			}
+			case "showmap": {
+				showMap();
+				break;
+			}
+
+			default: {
+				System.out.println("Invalid Command. Type Again.");
+			}
+			}
+
+			command = brConsole.readLine().split(" ");
+
+		}
+
+		System.out.println("savemap found");
+
+	}
+
+	private static Object readCommand() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static void showMap() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void editNeighbour(String[] command) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void editCountry(String[] command) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void editContinent(String[] command) {
+		// TODO Auto-generated method stub
 
 	}
 
