7de2cf90e32eda1849981018a34b501c8c13a052
divyabhosale <divyabhosale7@gmail.com>
2019-10-15 17:41:26 -0400

    Added UML and Test

---
+++ UnitTests/Game/MapReaderTest.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package Game;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author divya_000
+ *
+ */
+public class MapReaderTest {
+
+	Map expectedMap;
+	
+	
+	
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	
+	
+	
+	
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link Game.MapReader#validateMap()}.
+	 */
+	@Test
+	public void testValidateMap() {
+	
+	}
+
+}
