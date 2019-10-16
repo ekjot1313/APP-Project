8e06d4f1b189d6f48a5751b1ee6012526d591cc9
Hartaj <hartajsingh4@gmail.com>
2019-10-15 20:03:16 -0400

    Player Javadoc

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -21,16 +21,16 @@ public class Player {
 	}
 
 	/**
-	 *  This method returns the .
-	 * @return 
+	 *  This method returns the number of unassigned armies.
+	 * @return Number of Unassigned Armies
 	 */
 	public int getUnassignedarmies() {
 		return unassignedarmies;
 	}
 
 	/**
-	 *  This method returns the name of the player.
-	 * @param
+	 *  This method sets the number of unassigned armies
+	 * @param unassignedarmies Number of unassigned armies
 	 */
 
 	public void setUnassignedarmies(int unassignedarmies) {
@@ -39,42 +39,47 @@ public class Player {
 
 	/**
 	 *  This method returns the name of the player.
-	 * @return
+	 * @return Player Name
 	 */
 	public String getName() {
 		return name;
 	}
+	
 	/**
 	 * This method sets the name of the player.
-	 * @param name
+	 * @param name Player Name
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	/**
-	 *  This method returns the no of armies for a player.
-	 * @return
+	 *  This method returns the number of armies for a player.
+	 * @return Number of armies for a player
 	 */
 	public int getNoOfArmies() {
 		return noOfArmies;
 	}
+	
 	/**
-	 * This method sets the no of armies for a player.
-	 * @param noOfArmies
+	 * This method sets the number of armies for a player.
+	 * @param noOfArmies Number of armies of a particular player
 	 */
 	public void setNoOfArmies(int noOfArmies) {
 		this.noOfArmies = noOfArmies;
 	}
+	
 	/**
 	 * This method returns the list of countries belonging to a player.
-	 * @return
+	 * @return List of countries belonging to a player
 	 */
 	public List<Country> getAssigned_countries() {
 		return assigned_countries;
 	}
+	
 	/**
 	 * This method sets the list of countries belonging to a player.
-	 * @param assigned_countries
+	 * @param assigned_countries Countries assigned to a player
 	 */
 	public void setAssigned_countries(List<Country> assigned_countries) {
 		this.assigned_countries = assigned_countries;
