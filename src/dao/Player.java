8a00602c20d5d9cb7a5df0fdfc1d9162e3fabc91
Piyushpku <55817025+Piyushpku@users.noreply.github.com>
2019-10-30 15:07:24 -0400

    Refactoring- moving fortification and reinforcement in Player class

--- src/dao/Player.java
+++ src/dao/Player.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.util.*;
 import dao.Map;
+import mapWorks.MapReader;
 
 /**
  * This class represents the Player details
@@ -146,6 +147,190 @@ public class Player {
 		this.assigned_countries = assigned_countries;
 	}
 	/**
+	 * This method calculates the number of reinforcement armies
+	 * 
+	 * @param listPlayer  List of Players
+	 * @param map         Map Object
+	 * @param playerIndex Index of the player in the list
+	 * @return Number of reinforcement armies
+	 */
+	public int calculateReinforceArmies(Map map,ArrayList<Player> listPlayer) {
+		int noOfArmies =this.getAssigned_countries().size() / 3;
+		int reinforcementArmies = noOfArmies <= 3 ? 3 : noOfArmies;
+		return reinforcementArmies;
+	}
+
+	/**
+	 * This method is used for reinforcement phase.
+	 * 
+	 * @param listPlayer  List of players
+	 * @param map         Map object which contains the map details like continents
+	 *                    and countries.
+	 * @param playerIndex Index of a particular player in the List of player passed
+	 *                    from main function
+	 */
+	public void reinforcement(Map map,ArrayList<Player> listPlayer) {
+
+		Scanner sc = new Scanner(System.in);
+		// calculate reinforcement armies
+		int reinforcementArmies = calculateReinforceArmies(map,listPlayer);
+
+		// loop over playerlist and assign reinforcement armies
+		while (reinforcementArmies != 0) {
+			System.out.println("Reinforcement armies to be assigned :" + reinforcementArmies);
+			System.out.println("Type reinforce <countryname> <num>  to assign armies\n Type showmap");
+			String input = sc.nextLine();
+			String[] inputArray = input.split(" ");
+			if (input.equals("showmap")) {
+				MapReader mr = new MapReader();
+				
+				map.displayAll();
+			} else if (inputArray.length == 3 && inputArray[0].equals("reinforce")) {
+				int armiesTobeplaced = Integer.parseInt(inputArray[2]);
+				int countryFound = 0;
+
+				ArrayList<Country> countryTempList = (ArrayList<Country>) this.getAssigned_countries();
+				for (Country c : countryTempList) {
+					if (c.getName().equals(inputArray[1]))
+						countryFound = 1;
+				}
+				if (countryFound == 0)
+					System.out.println("Country is not assigned to player ");
+				else {
+					if (armiesTobeplaced <= reinforcementArmies && armiesTobeplaced > 0) { // check reinforce command
+																							// and country is valid and
+																							// assigned to player
+
+						this.setNoOfArmies(this.getNoOfArmies() + armiesTobeplaced);
+						map.getCountryFromName(inputArray[1]).setNoOfArmies(
+								map.getCountryFromName(inputArray[1]).getNoOfArmies() + armiesTobeplaced);
+						reinforcementArmies -= armiesTobeplaced;
+						System.out.println("Reinforcement armies placed successfully");
+					} else
+						System.out.println(
+								"Number of armies to be assigned should be in the range : 1 -" + reinforcementArmies);
+				}
+			} else
+				System.out.println("Invalid command .Type again");
+		}
+		
+	}
+
+	/**
+	 * This function is used for fortification phase.
+	 * 
+	 * @param listPlayer  List of players
+	 * @param map         Map object which contains the map details like continents
+	 *                    and countries.
+	 * @param playerIndex Index of a particular player in the List of player passed
+	 *                    from main function
+	 */
+	public void fortification(Map map,ArrayList<Player> listPlayer) {
+		Scanner sc = new Scanner(System.in);
+		int flag = 0;
+		do {
+
+			System.out.println(
+					"Type fortify <from country name> <to country name> <number of armies> or fortify none (choose to not do a move)\n Type showmap");
+			String in = sc.nextLine();
+			String input[] = in.split(" ");
+			if (in.equals("showmap")) {
+				MapReader mr = new MapReader();
+				map.displayAll();
+			} else if (input.length == 4 && input[0].equals("fortify")) {
+				HashMap<Integer, List<Integer>> mapOfAssignedCountries = new HashMap<Integer, List<Integer>>();
+				for (int i = 0; i < this.getAssigned_countries().size(); i++) {
+					List<Integer> templist = new ArrayList<Integer>();
+					for (int j = 0; j <this.getAssigned_countries().get(i).getNeighbors()
+							.size(); j++) {
+						if (map.getCountryFromName(
+								this.getAssigned_countries().get(i).getNeighbors().get(j))
+								.getOwner().equals(this.getName())) {
+							for (int k = 0; k < this.getAssigned_countries().size(); k++) {
+								if (this.getAssigned_countries().get(k)
+										.equals(map.getCountryFromName(this.getAssigned_countries().get(i).getNeighbors().get(j)))) {
+									templist.add(k);
+								}
+							}
+						}
+					}
+					mapOfAssignedCountries.put(i, templist);
+				}
+				int source = -1, destination = -1, validPath = 0;
+				for (int k = 0; k < this.getAssigned_countries().size(); k++) {
+					if (this.getAssigned_countries().get(k).getName().equals(input[1])) {
+						source = k;
+					}
+					if (this.getAssigned_countries().get(k).getName().equals(input[2])) {
+						destination = k;
+					}
+				}
+				if (source == -1 || destination == -1) {
+					if (source == -1 && destination == -1)
+						System.out.println("Sorry!From Country:" + input[1] + " and To Country :" + input[2]
+								+ " doesn't belong to you");
+					else if (source == -1)
+						System.out.println("Sorry!From Country :" + input[1] + " doesn't belong to you");
+					else
+						System.out.println("Sorry!To Country :" + input[2] + " doesn't belong to you");
+				} else {
+					Boolean[] visited = new Boolean[mapOfAssignedCountries.keySet().size()];
+					for (int i = 0; i < visited.length; i++) {
+						visited[i] = false;
+					}
+					LinkedList<Integer> queue = new LinkedList<Integer>();
+					queue.add(source);
+					visited[source] = true;
+					while (queue.size() > 0) {
+						Integer c1 = queue.poll();
+						Iterator<Integer> i = mapOfAssignedCountries.get(c1).listIterator();
+						while (i.hasNext()) {
+							int n = (int) i.next();
+
+							if (n == destination) {
+								validPath = 1;
+								break;
+							}
+							if (visited[n] == false) {
+								visited[n] = true;
+								queue.add(n);
+							}
+						}
+						if (validPath == 1)
+							break;
+					}
+					if (validPath == 1) {
+						if (Integer.parseInt(input[3]) > 0 && (Integer.parseInt(input[3]) < this.getAssigned_countries().get(source).getNoOfArmies())) {
+							this.getAssigned_countries().get(source).setNoOfArmies(
+									(this.getAssigned_countries().get(source).getNoOfArmies())
+											- Integer.parseInt(input[3]));
+							this.getAssigned_countries().get(destination)
+									.setNoOfArmies(this.getAssigned_countries().get(destination)
+											.getNoOfArmies() + Integer.parseInt(input[3]));
+							System.out.println("Fortification successful");
+							
+							return;
+						} else
+							System.out.println("Invalid no of armies specified, for these two countries it can be 1-"
+									+ (this.getAssigned_countries().get(source).getNoOfArmies()
+											- 1));
+					} else {
+						System.out.println(
+								"There's no path between the mentioned countries.(You can move any number of armies from one of the owned countries to the other, provided that there is a path between these two countries that is composed of countries owned by you)");
+					}
+				}
+			} else if (in.equals("fortify none")) {
+				System.out.println("Skipped fortification");
+				
+				return;
+			} else
+				System.out.println("Invalid command,type again");
+		} while (flag == 0);
+
+		
+	}
+
+	/**
 	 * This method is used for attack phase.
 	 */
 	public void attack(Map map,ArrayList<Player> listPlayer) {
@@ -168,7 +353,11 @@ public class Player {
 				int validCommand=0;
 				String defend=toCountry.getOwner();
 				Player defender=new Player();
-				
+				for(Player p:listPlayer) {
+					if(p.getName().equals(s[2])) {
+						
+					}
+				}
 				System.out.println("Player :"+defend+" has to defend country :"+s[2]+" \nType defend numdice to choose no of dices to defend your country.");
 				input=sc.nextLine();
 				String str[]=input.split(" ");
