a5262665f473e242061439c8a14e7e97f722df47
divyabhosale <divyabhosale7@gmail.com>
2019-11-03 17:43:17 -0500

    Observable update

--- src/pattern/Observable.java
+++ src/pattern/Observable.java
@@ -2,18 +2,37 @@ package pattern;
 
 import java.util.ArrayList;
 
+/**
+ * This is the Observable class which will be extended by the subject.
+ * @author divya_000
+ *
+ */
 public class Observable {
 
-	ArrayList observers = new ArrayList<>();
+	/**
+	 * @param observers arraylist of observer objects
+	 */
+	ArrayList<Observer> observers = new ArrayList<Observer>();
 	
+	/**
+	 * This method attaches the observer to observable class
+	 * @param o object of observer class
+	 */
 	public void attach(Observer o) {
 		observers.add(o);
 	}
-	
+	/**
+	 * This method detaches the observer from observable class
+	 * @param o object of observer class
+	 */
 	public void detach(Observer o) {
 		observers.remove(o);
 	}
 	
+	/**
+	 * This method notifies the attached observers
+	 * @param obj object of observable class
+	 */
 	public void notify(Observable obj) {
 		for(int i=0;i<observers.size();i++) {
 			((Observer) observers.get(i)).update(obj);
--- src/pattern/Observer.java
+++ src/pattern/Observer.java
@@ -1,6 +1,14 @@
 package pattern;
-
+/**
+ * This is interface which is implemented by observer class
+ * @author divya_000
+ *
+ */
 public interface Observer {
 
+	/**
+	 * This method updates the observer 
+	 * @param obj object of observable object
+	 */
 	public void update(Observable obj);
 }
