293ccfb1ec9fac3dbf2a5b18d22aff87c6d3df46
divyabhosale <divyabhosale7@gmail.com>
2019-11-07 15:46:08 -0500

    Card Exchange Error resolved

--- src/test/MapWorksTestSuite.java
+++ src/test/MapWorksTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({MapReaderTest.class, MapSaverTest.class})
+@SuiteClasses({MapReaderTest.class, MapSaverTest.class, MapEditorTest.class})
 public class MapWorksTestSuite {
 
 }
