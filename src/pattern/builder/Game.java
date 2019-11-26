47593424a920b5471afe62bea1e149b2f078ff4c
divyabhosale <divyabhosale7@gmail.com>
2019-11-26 15:37:14 -0500

    Game in builder

--- src/pattern/builder/Game.java
+++ src/pattern/builder/Game.java
@@ -1,5 +1,48 @@
 package pattern.builder;
 
+import dao.Map;
+
+/**
+ * This is the product class of builder pattern
+ * @author divya_000
+ *
+ */
 public class Game {
 
+	/**
+	 * Object of Map class
+	 */
+	private Map map;
+
+	/**
+	 * To store the current player
+	 */
+	private String currentPlayer;
+
+	/**
+	 * To store the current phase
+	 */
+	private String currentPhase;
+	
+	/**
+	 * Method to set the map object
+	 * @param map object of Map
+	 */
+	public void setMap(Map map) {
+		this.map = map;
+	}
+	/**
+	 * Method to set the current player
+	 * @param currentPlayer
+	 */
+	public void setCurrentPlayer(String currentPlayer) {
+		this.currentPlayer = currentPlayer;
+	}
+	/**
+	 * Method to set the current phase
+	 * @param currentPhase
+	 */
+	public void setCurrentPhase(String currentPhase) {
+		this.currentPhase = currentPhase;
+	}
 }
