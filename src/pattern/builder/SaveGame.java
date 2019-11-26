0ea0d8ee3c7275b66829c278a43d8fe296f265dd
Mitzn1104 <55814791+Mitzn1104@users.noreply.github.com>
2019-11-26 15:38:08 -0500

    gb

--- src/pattern/builder/SaveGame.java
+++ src/pattern/builder/SaveGame.java
@@ -1,5 +1,25 @@
 package pattern.builder;
 
-public class SaveGame {
+import dao.Map;
+
+public class SaveGame extends GameBuilder {
+
+	@Override
+	void buildMap(String filename, Map map) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void buildPlayer(String filename, Map map) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void buildPhase(String filename, Map map) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
