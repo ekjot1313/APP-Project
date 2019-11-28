5ec7be99af88e9cb7f5d9b81ba00d443a8e0d705
divyabhosale <divyabhosale7@gmail.com>
2019-11-27 19:18:56 -0500

    Builder Test suite

--- src/test/builder/BuilderTestSuite.java
+++ src/test/builder/BuilderTestSuite.java
@@ -1,11 +1,10 @@
-package test;
+package test.builder;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import test.GameEngine.GameEngineTestSuite;
-import test.builder.BuilderTestSuite;
 import test.daotest.DaoTestSuite;
 import test.gametest.GameTestSuite;
 import test.mapworkstest.MapWorksTestSuite;
@@ -13,6 +12,7 @@ import test.pattern.Strategy.StrategyTestSuite;
 import test.viewtest.ViewTestSuite;
 
 @RunWith(Suite.class)
-@SuiteClasses({DaoTestSuite.class, MapWorksTestSuite.class, GameTestSuite.class, ViewTestSuite.class ,StrategyTestSuite.class , GameEngineTestSuite.class, BuilderTestSuite.class})
-public class AllTests {
+@SuiteClasses({LoadGameBuilderTest.class, SaveGameBuilderTest.class})
+public class BuilderTestSuite {
+
 }
